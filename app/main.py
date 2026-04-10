8824ac29a141401eed2e510e96bb1025f50da82e
Vincent <zayncaster24@gmail.com>
2026-04-10 07:49:25 +0700

    fix

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from app.api.endpoints import chat
 from app.core.database import engine, Base
 import os
